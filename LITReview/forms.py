36b3257fdced0ae9e619862a1a8f608e18430904
NVendrisse <92920252+NVendrisse@users.noreply.github.com>
2023-07-25 17:00:59 +0200

    Delete LITReview directory

--- LITReview/forms.py
+++
@@ -1,9 +0,0 @@
-from django import forms
-from django.contrib.auth import get_user_model
-
-user = get_user_model()
-
-class SocialForm(forms.ModelForm):
-    class Meta:
-        model = user
-        fields = ['follows']
